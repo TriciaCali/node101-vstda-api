96e9b3b09a0ac2b387ae018e039f160886c6df61
Latricia Hunter <latricia@Latricias-iMac.local>
2022-05-20 11:34:18 -0700

    Project Completed. All requests are working (get,post and delet)

--- server/app.js
+++ server/app.js
@@ -1,11 +1,33 @@
 const express = require('express');
 const morgan = require('morgan');
-const data = require('../server/data.json');
+//const data = require('./server/data.json');
 var bodyParser = require('body-parser');
 const res = require('express/lib/response');
-
+const fs = require('fs');
+const { builtinModules } = require('module');
+var fsData = fs.readFileSync('./server/data.json');
+var dataObject = JSON.parse(fsData);
 const app = express();
 const router = express.Router();
+
+let data = [{
+    "todoItemId": 0,
+    "name": "an item",
+    "priority": 3,
+    "completed": false
+},
+{
+    "todoItemId": 1,
+    "name": "another item",
+    "priority": 2,
+    "completed": false
+},
+{
+    "todoItemId": 2,
+    "name": "a done item",
+    "priority": 1,
+    "completed": true
+}]
 
 // apply middleware
 app.use(morgan('dev'))
@@ -26,27 +48,23 @@ app.use((req,res, next)=>{
 // BODY: {status: ok}
 app.get('/', (req, res)=>{
     res.status(200).json({"status":"ok"})
-    console.log( 'ln 29 request from /')
     next();
 });
-
-
 
 // GET REQUEST: route: /api/TodoItems  
 // GET response: array of ALL objects (jsonFile) 
 // successful request: status code 200
 app.get('/api/TodoItems', (req, res)=>{
     res.status(200).json(data)
-    console.log( 'ln 40 request from /api/TodoItems')
 });
-
 
 //GET REQUEST: route: /api/TodoItems/:number
 // GET response: object of specific todo requested
 //successful request: status code 200
-app.get('/api/TodoItems/:number', (req,res)=>{
-    console.log("ln 41 req.params.number =  ")
-    res.status(200).json(data[req.params.number]);
+app.get('/api/TodoItems/:id', (req,res)=>{
+    let index3 = data.findIndex(el=> el.todoItemId == req.params.id);
+    let id = data[index3]
+    res.status(200).json(id);
 });
 
 
@@ -57,27 +75,43 @@ app.get('/api/TodoItems/:number', (req,res)=>{
 //      repoonse: copy of item posted
 //      (common to send back copy that was posted in body of response)
 //successful request: status code 201
-app.post('/api/TodoItems', (req,res)=>{
-    if (data.match(req.body.todoItemID)){
-        //change data todoItemID values to the body.todoItem values to overwrite preexisting data
+ app.post('/api/TodoItems', (req,res)=>{
+     
+    if(fsData != req.body){
+        //add data to file
+        var newData2 = JSON.stringify(req.body)
+        fs.writeFile('data.json', newData2, err=>{
+            if(err)throw err;
+            console.log("new data added")
+        })
+    }else{
+        //update the arry that has the same req.body.todoItemID
+        let index1 = data.findIndex(el=> el.todoItemId == req.body.todoItemId);
+        data[index1].name = req.body.name;
+        data[index1].priority = req.body.priority;
+        data[index1].completed = req.body.completed;
+
     }
-    else{
-        //append body data
-    }
+
+    /*console.log("ln 61 req.body.todoItemID  "+ req.body.todoItemID)
+    data[todoItemID] =  req.body.todoItemID;
+    data["name"] = req.body.name;
+    data["priority"] = req.body.priority;
+    data["completed"] = req.body.completed; */
+    
  //show what was added
     res.status(201).json(req.body);
 })
 
-
-
 //DELETE REQUEST: /api/TodoItems/:number
 //DELETE RESPONSE: copy of item deleted (BODY: {item})
 //successful request: status code 200
-app.delete('/api/TodoItems/:number', (req, res)=>{
- let item = data[req.params.id]
- data.splice(req.params.id, 1);
+app.delete('/api/TodoItems/:id', (req, res)=>{
+let toBeDeleted = data[req.params.id]
+ let index = data.findIndex(el=> el.todoItemId == req.params.id)
+  data.splice(index, 1);
  //show what was deleted
-res.status(200).json(item);
+res.status(200).json(toBeDeleted);
 })
 
 module.exports = app;
--- server/index.js
+++ server/index.js
@@ -3,6 +3,6 @@ const server = require('./app');
 // write your code here
 //tell app to listen for network reqest on port 8484
 
-server.listen(8484, function() {
+server.listen(8080, function() {
     console.log('Server is listening on http://localhost:8080');
 })
